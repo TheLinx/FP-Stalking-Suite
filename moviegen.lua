2827bc1570f636ff3b1631ec6f068578a5f09120
Linus Sjögren <thelinx@unreliablepollution.net>
2010-03-30 22:35:43 +0200

    added 'auto' date.

--- moviegen.lua
+++ moviegen.lua
@@ -1,19 +1,23 @@
 local lfs = require"lfs"
 
 if arg then
-	day = arg[1] or nil
-	cam = arg[2] or nil
+    if arg[1] == "auto" then
+        day = os.date("%y-%m-%d")
+    else
+        day = arg[1] or nil
+    end
+    cam = arg[2] or nil
 end
 
 if not day then
-	io.write("Choose a date: ")
-	day = io.read("*l")
+    io.write("Choose a date: ")
+    day = io.read("*l")
 end
 if not lfs.attributes(day) then print("You must choose a valid date!") error() end
 
 if not cam then
-	io.write("Choose a camera: ")
-	cam = io.read("*l")
+    io.write("Choose a camera: ")
+    cam = io.read("*l")
 end
 
 os.execute("mencoder -mf w=640:h=480:fps=10 -ovc copy -o C"..cam.."-"..day..".avi mf://"..day.."/C"..cam.."*.jpg")
